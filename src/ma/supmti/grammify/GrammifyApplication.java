8c41d0dfb0dbe340eda49d7b55349704dca6236c
xAyBix <akrambelbekri@gmail.com>
2025-11-16 05:10:04 +0100

    project base

--- src/ma/supmti/grammify/GrammifyApplication.java
+++ src/ma/supmti/grammify/GrammifyApplication.java
@@ -1,5 +1,10 @@
 package ma.supmti.grammify;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+
 import javax.swing.JFrame;
 
 
@@ -10,13 +15,14 @@ import javax.swing.JFrame;
  * @author Rihab AMEUR
  * @author Hidaya EL ARBAOUI
  * 
- * @since 2025-11-15 21:30
+ * @since 2025-11-15 22:30
  */
 public class GrammifyApplication {
 	public static void main(String[] args) {
 		
 		// Interface's frame configuration
 		JFrame appFrame = new JFrame(Constants.APP_NAME);
+		appFrame.setIconImage(Constants.img);
 		appFrame.setSize(Constants.FRAME_WIDTH, Constants.FRAME_HEIGHT);
 		appFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		appFrame.setLocationRelativeTo(null);
