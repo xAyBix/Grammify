dce69847658012466587696e4a30e82a45a93e71
rihab <ameurrihab7@gmail.com>
2025-11-20 15:09:09 +0100

    Feat: Adding open and save methods to FileManager

--- src/ma/supmti/grammify/io/FileManager.java
+++ src/ma/supmti/grammify/io/FileManager.java
@@ -1,5 +1,11 @@
 package ma.supmti.grammify.io;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 /**
  * 
  * 
@@ -9,6 +15,30 @@ package ma.supmti.grammify.io;
  * 
  * @since 2025-11-19 16:33
  */
-public class FileManager {
+public final class FileManager {
 
+    public static String open(String path) {
+        String text = "";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                text += line;
+            }
+            reader.close();
+            return text;
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public static void save(String text, String path) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+            writer.write(text);
+            writer.close();
+        } catch (Exception e) {
+        }
+
+    }
 }
