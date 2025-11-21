6b31d7a00a64fb026c438f47300402e6d8b05d29
xAyBix <akrambelbekri@gmail.com>
2025-11-21 11:30:32 +0100

    Refactor: Renaming OpenedFile

---
+++ src/ma/supmti/grammify/io/OpenedFile.java
@@ -0,0 +1,19 @@
+package ma.supmti.grammify.io;
+
+import java.io.File;
+
+/**
+ * Contains all currently opened file infos needed
+ * 
+ * 
+ * 
+ * @author Rihab AMEUR
+ * 
+ * @since 2025-11-21 11:27
+ */
+public final class OpenedFile {
+	public static File file;
+	public static String name;
+	public static String path;
+	public static String initialText;
+}
